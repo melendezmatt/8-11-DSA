16a39af85b699b0039af0079ad620cd1630525e7
Matt Melendez <matthewemelendez@gmail.com>
2021-08-11 22:57:23 -0700

    first commit

---
+++ practice.js
@@ -0,0 +1,21 @@
+
+// Have the function BracketCombinations(num) read num which will be an integer greater than or equal to zero,
+// and return the number of valid combinations that can be formed with num pairs of parentheses. For example, if
+// the input is 3, then the possible combinations of 3 pairs of parenthesis, namely: ()()(), are ()()(), ()(()),
+// (())(), ((())), and (()()). There are 5 total combinations when the input is 3, so your program should return 5.
+
+// Input: 3
+// Output: 5
+
+// Input: 2
+// Output: 2
+
+function BracketCombinations(num) {
+
+    // code goes here
+    return num;
+
+  }
+
+  // keep this function call here
+  console.log(BracketCombinations(readline()));
